c03f9de925265342bbfcf30837b5ded45c28af58
daniel Woodford <daniel.woodford.g@gmail.com>
2019-08-12 11:52:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,30 @@
 require 'pry'
+class School
+  
+  attr_accessor :name, :roster
 
-class School 
+  def initialize(name)
+      @name = name
+      @roster = {}
+    end
+  
+  def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end 
 
-    
-end #class end 
+  def grade(grade_of_students) 
+    roster[grade_of_students]
+  end 
+  
+  def sort
+    sorter = {}
+   ham = roster.sort
+     ham.each do |x, y|
+       sorter[x] = y.sort
+       #binding.pry
+     end
+    sorter
+  end
 
-
+end #class end
